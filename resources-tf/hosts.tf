2965947bf86cd6a3a76cc739497a039dac74bfb0
Tomáš Virtus <tomas.virtus@rchain.coop>
2019-03-26 06:51:48 +0100

    Initial commit

---
+++ resources-tf/hosts.tf
@@ -0,0 +1,59 @@
+resource "google_compute_address" "node_ext_addr" {
+  count = "${var.node_count}"
+  name = "${var.resources_name}-node${count.index}"
+  address_type = "EXTERNAL"
+}
+
+resource "google_dns_record_set" "node_dns_record" {
+  count = "${var.node_count}"
+  name = "node${count.index}${var.dns_suffix}."
+  managed_zone = "rchain-dev"
+  type = "A"
+  ttl = 300
+  rrdatas = ["${google_compute_address.node_ext_addr.*.address[count.index]}"]
+}
+
+resource "google_compute_instance" "node_host" {
+  count = "${var.node_count}"
+  name = "${var.resources_name}-node${count.index}"
+  hostname = "node${count.index}${var.dns_suffix}"
+  machine_type = "custom-4-16384"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-1810"
+      size = 160
+      type = "pd-ssd"
+    }
+  }
+
+  tags = [
+    "${var.resources_name}-node-public",
+    "${var.resources_name}-node-grpc",
+    "collectd-out",
+    "elasticsearch-out",
+    "logstash-tcp-out"
+  ]
+
+  network_interface {
+    network = "${data.google_compute_network.default_network.self_link}"
+    access_config {
+      nat_ip = "${google_compute_address.node_ext_addr.*.address[count.index]}"
+    }
+  }
+
+  connection {
+    type = "ssh"
+    user = "root"
+    private_key = "${file("~/.ssh/google_compute_engine")}"
+  }
+
+  provisioner "file" {
+    source = "${var.rchain_sre_git_crypt_key_file}"
+    destination = "/root/rchain-sre.git-crypt-key"
+  }
+
+  provisioner "remote-exec" {
+    script = "../host-update-run-setup"
+  }
+}
---
+++ resources-tf/main.tf
@@ -0,0 +1,15 @@
+variable "resources_name" { default = "testnet1" }
+variable "gcp_zone" { default = "us-central1-a" }
+variable "node_count" { default = 10 }
+variable "dns_suffix" { default = ".testnet.rchain-dev.tk" }
+variable "rchain_sre_git_crypt_key_file" {}
+
+provider "google" {
+  project = "developer-222401"
+  zone    = "${var.gcp_zone}"
+}
+
+provider "google-beta" {
+  project = "developer-222401"
+  zone    = "${var.gcp_zone}"
+}
