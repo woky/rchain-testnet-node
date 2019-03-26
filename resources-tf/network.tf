0c78d7d95854f149171a921636cbf587ecbb1b67
Tomáš Virtus <tomas.virtus@rchain.coop>
2019-03-26 12:56:20 +0100

    Remove bootstrap CNAME

--- resources-tf/network.tf
+++ resources-tf/network.tf
@@ -23,12 +23,3 @@ resource "google_compute_firewall" "fw_public_node_rpc" {
     ports = [ 40401 ]
   }
 }
-
-resource "google_dns_record_set" "bootstrap_dns_record" {
-  name = "bootstrap${var.dns_suffix}"
-  managed_zone = "rchain-dev"
-  type = "CNAME"
-  ttl = 300
-  rrdatas = ["node0${var.dns_suffix}"]
-}
-
