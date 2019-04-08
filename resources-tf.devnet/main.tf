5b1149db2797a9f170fc88f376356aa93ea17359
Tomáš Virtus <tomas.virtus@rchain.coop>
2019-04-09 00:59:28 +0200

    Add devnet profile files

---
+++ resources-tf.devnet/main.tf
@@ -0,0 +1,15 @@
+variable "resources_name" { default = "devnet" }
+variable "gcp_zone" { default = "us-east1-b" }
+variable "node_count" { default = 6 }
+variable "dns_suffix" { default = ".devnet.rchain-dev.tk" }
+variable "rchain_sre_git_crypt_key_file" {}
+
+provider "google" {
+  project = "developer-222401"
+  zone    = "${var.gcp_zone}"
+}
+
+provider "google-beta" {
+  project = "developer-222401"
+  zone    = "${var.gcp_zone}"
+}
---
+++ resources-tf.devnet/network.tf
@@ -0,0 +1,25 @@
+data "google_compute_network" "default_network" {
+  name = "default"
+}
+
+resource "google_compute_firewall" "fw_public_node" {
+  name = "${var.resources_name}-node-public"
+  network = "${data.google_compute_network.default_network.self_link}"
+  priority = 530
+  target_tags = [ "${var.resources_name}-node-public" ]
+  allow {
+    protocol = "tcp"
+    ports = [ 50400, 40403, 50404 ]
+  }
+}
+
+resource "google_compute_firewall" "fw_public_node_rpc" {
+  name = "${var.resources_name}-node-rpc"
+  network = "${data.google_compute_network.default_network.self_link}"
+  priority = 540
+  target_tags = [ "${var.resources_name}-node-rpc" ]
+  allow {
+    protocol = "tcp"
+    ports = [ 40401 ]
+  }
+}
