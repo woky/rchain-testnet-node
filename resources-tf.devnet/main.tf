a279992cdbc10d899e2bdf4645c5cc02b0c7a4e6
Tomáš Virtus <tomas.virtus@rchain.coop>
2019-04-09 01:19:38 +0200

    Reduce devnet to 5 nodes (GCP IP address limit)

--- resources-tf.devnet/main.tf
+++ resources-tf.devnet/main.tf
@@ -1,6 +1,6 @@
 variable "resources_name" { default = "devnet" }
 variable "gcp_zone" { default = "us-east1-b" }
-variable "node_count" { default = 6 }
+variable "node_count" { default = 5 }
 variable "dns_suffix" { default = ".devnet.rchain-dev.tk" }
 variable "rchain_sre_git_crypt_key_file" {}
 
