d245b551eba3775e444fbe89af20d9280b288d05
Tomáš Virtus <tomas.virtus@rchain.coop>
2019-04-11 02:54:41 +0200

    devnet: Use default protocol/kademlia ports (40400/40404)

--- resources-tf.devnet/network.tf
+++ resources-tf.devnet/network.tf
@@ -9,7 +9,7 @@ resource "google_compute_firewall" "fw_public_node" {
   target_tags = [ "${var.resources_name}-node-public" ]
   allow {
     protocol = "tcp"
-    ports = [ 50400, 40403, 50404, 18080 ]
+    ports = [ 40400, 40403, 40404, 18080 ]
   }
 }
 
