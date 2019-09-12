c4dd650e7b43fc9a1bacea762184bc30d600d063
Adam Szkoda <adaszko@gmail.com>
2019-09-12 14:54:40 +0200

    Call deploy and propose shell commands directly

--- autopropose.py
+++ autopropose.py
@@ -1,68 +1,134 @@
 #!/usr/bin/env python3
 
-import os
 import time
 import logging
 import subprocess
+import contextlib
+
+from typing import (
+    Dict,
+    Tuple,
+)
+
+from ecdsa import SigningKey
+from ecdsa.curves import SECP256k1
 
 
 logging.basicConfig(level=logging.INFO, format='%(levelname)s %(message)s')
 LOGGER = logging.getLogger()
 
 
-def parse_conf(contents):
+def generate_key_pair_hex() -> Tuple[str, str]:
+    signing_key = SigningKey.generate(curve=SECP256k1)
+    verifying_key = signing_key.get_verifying_key()
+    signing_key_hex = signing_key.to_string().hex()
+    verifying_key_hex = "04" + verifying_key.to_string().hex()
+    return (signing_key_hex, verifying_key_hex)
+
+
+def generate_keypair() -> Tuple[str, str]:
+    (private_key, public_key) = generate_key_pair_hex()
+    return (private_key, public_key)
+
+
+def parse_conf(contents: str) -> Dict[str, str]:
     result = {}
     for line in contents.splitlines():
         if line == '':
             continue
         key, value = line.split('=', maxsplit=1)
-        value = value.strip('"')
-        value = value.strip("'")
+        value = value.strip('"').strip("'")
         result[key] = value
     return result
 
 
-def parse_conf_file(file_name):
+def parse_conf_file(file_name: str) -> Dict[str, str]:
     with open(file_name) as file:
         contents = file.read()
         return parse_conf(contents)
 
 
-def main():
-    config = parse_conf_file('/var/lib/rnode-static/autopropose.conf')
-    deploy_no_sooner_than_every = int(config.get('period', '60'))
+def deploy(contract: str, private_key: str) -> None:
+    deploy_command = [
+        'docker', 'exec', 'rnode',
+        './bin/rnode',
+        '-c', '/var/lib/rnode-static/rnode.conf',
+        'deploy',
+        '--phlo-limit', '1000000000',
+        '--phlo-price', '1',
+        '--private-key', private_key,
+        contract,
+    ]
 
-    deploy_env = {}
-    if 'contract' in config:
-        deploy_env['RD_OPTION_CONTRACT'] = config['contract']
+    try:
+        LOGGER.info("Deploying %s", contract)
+        subprocess.check_output(deploy_command)
+    except subprocess.CalledProcessError as e:
+        LOGGER.error("Command: %s", e.cmd)
+        LOGGER.exception('deploy')
+        LOGGER.error("Exit code: %d", e.returncode)
+        LOGGER.error("Stdout: %s", e.stdout)
+        LOGGER.error("Stderr: %s", e.stderr)
 
-    LOGGER.info('Deploying contract %s every %d seconds', deploy_env.get('RD_OPTION_CONTRACT'), deploy_no_sooner_than_every)
 
-    while True:
-        while True:
-            rnode_container_hash = subprocess.check_output(['docker', 'ps', '--quiet', '--filter=name=^rnode$'])
-            if rnode_container_hash != '':
-                break
-            time.sleep(15)
 
-        try:
-            deploy_started_at = int(time.monotonic())
-            LOGGER.info("Deploying %s", deploy_env.get('RD_OPTION_CONTRACT'))
-            subprocess.check_output(['/opt/rchain-testnet-node/rundeck-scripts/deploy'], env=deploy_env)
-        except subprocess.CalledProcessError:
-            LOGGER.exception('deploy')
+def propose():
+    propose_command = [
+        'docker',
+        'exec',
+        'rnode',
+        './bin/rnode',
+        '-c', '/var/lib/rnode-static/rnode.conf',
+        'propose',
+    ]
 
-        deploy_finished_at = int(time.monotonic())
-        deploy_took = deploy_finished_at - deploy_started_at
-        if deploy_took < deploy_no_sooner_than_every:
-            wait_duration = deploy_no_sooner_than_every - deploy_took
+    try:
+        LOGGER.info("Proposing...")
+        subprocess.check_output(propose_command)
+    except subprocess.CalledProcessError as e:
+        LOGGER.error("Command: %s", e.cmd)
+        LOGGER.exception('propose')
+        LOGGER.error("Exit code: %d", e.returncode)
+        LOGGER.error("Stdout: %s", e.stdout)
+        LOGGER.error("Stderr: %s", e.stderr)
+
+
+@contextlib.contextmanager
+def must_take_at_least(seconds):
+    started_at = int(time.monotonic())
+    try:
+        yield
+    finally:
+        finished_at = int(time.monotonic())
+        took = finished_at - started_at
+        if took < seconds:
+            wait_duration = seconds - took
             time.sleep(wait_duration)
 
-        try:
-            LOGGER.info("Proposing...")
-            subprocess.check_output(['/opt/rchain-testnet-node/rundeck-scripts/propose'])
-        except subprocess.CalledProcessError:
-            LOGGER.exception('propose')
+
+
+def main():
+    config = parse_conf_file('/var/lib/rnode-static/autopropose.conf')
+
+    contract = config.get('contract')
+    if contract is None or contract == '' or contract == 'none':
+        LOGGER.info('No contract configured.  Going to sleep...')
+        while True:
+            time.sleep(1)
+
+    deploy_no_sooner_than_every = int(config.get('period', '60'))
+    LOGGER.info('Deploying contract %s every %d seconds', config.get('contract'), deploy_no_sooner_than_every)
+    while True:
+        if subprocess.check_output(['docker', 'ps', '--quiet', '--filter=name=^rnode$']) == '':
+            time.sleep(15)
+            continue
+
+        with must_take_at_least(deploy_no_sooner_than_every):
+            private_key, _ = generate_keypair()
+            deploy(contract, private_key)
+
+        propose()
+
 
 if __name__ == '__main__':
     main()
