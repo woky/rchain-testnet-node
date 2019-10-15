aed3cf3ea5be881da7851aed08c01aeeb46f8928
Nutzipper <nomail@nodomain.nozone>
2019-10-15 09:01:30 +0300

    enable printing gRPC response

--- scripts/propose.py
+++ scripts/propose.py
@@ -1,16 +1,20 @@
 import grpc
 import logging
+import sys
 from rchain.client import RClient
 from rchain.client import RClientException
 
 root = logging.getLogger()
-root.setLevel(logging.INFO)
+root.setLevel(logging.DEBUG)
 
+ret=0
 with grpc.insecure_channel('localhost:40401') as channel:
     client = RClient(channel)
     try:
         client.propose()
     except RClientException as e:
-        # NoNewDeploys is a valid scenario
+        # NoNewDeploys case should not exit with an error \
+        # so autopropose script can deploy again before next propose
         if "NoNewDeploys" not in str(e):
-            raise
+            ret=1
+sys.exit(ret)
